2f5e620ada0703e93fb17411a88082a9a5ab2a55
gerrad <gerrad.meitamei@student.moringaschool.com>
2025-05-18 01:18:09 +0300

    initial commit

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = ""
+        self.value = value  # Use the setter
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+        else:
+            self._value = new_value
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        # Split on sentence-ending punctuation and count non-empty segments
+        import re
+        sentences = re.split(r'[.!?]', self._value)
+        return len([s for s in sentences if s.strip()])
